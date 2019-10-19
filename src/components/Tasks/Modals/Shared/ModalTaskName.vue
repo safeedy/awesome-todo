15f68ac8f6f2b6536a3a532c9d2dd9a23642c394
Safidy Ambinintsoa <sambinintsoa-ext@lequipe.fr>
2019-10-19 18:54:14 +0200

    directive select-all

--- src/components/Tasks/Modals/Shared/ModalTaskName.vue
+++ src/components/Tasks/Modals/Shared/ModalTaskName.vue
@@ -6,12 +6,25 @@
         @input="$emit('update:name', $event)"
         label="Task name" class="col"
         ref="name"
+        v-select-all
         :rules="[val => !!val || 'Field is required']"
         clearable/>
     </div>
 </template>
 <script>
 export default {
-    props: ['name']
+    props: ['name'],
+    directives: {
+        selectAll: {
+            inserted(el) {
+                let input = el.querySelector('.q-field__native')
+                input.addEventListener('focus', () => {
+                    if(input.value.length) {
+                        input.select()
+                    }
+                })
+            }
+        }
+    }
 }
 </script>
