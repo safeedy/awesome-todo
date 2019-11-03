05e4d9647c7e8ce33e2cf14308c1669625530abd
Safidy Ambinintsoa <sambinintsoa-ext@lequipe.fr>
2019-11-03 16:44:50 +0100

    persist settings in localStorage

--- src/store/store-settings.js
+++ src/store/store-settings.js
@@ -1,5 +1,4 @@
-import { uid } from 'quasar'
-import Vue from 'vue'
+import { LocalStorage } from 'quasar'
 
 const state = {
     settings: {
@@ -10,12 +9,30 @@ const state = {
 const mutations =  {
     setShow12HourTimeFormatMutation(state, value) {
         state.settings.show12HourTimeFormat = value
+    },
+    setSettings(state, settings) {
+        Object.assign(state.settings, settings)
     }
 }
 
 const actions = {
-    setShow12HourTimeFormat({ commit }, value) {
+    setShow12HourTimeFormat({ commit, dispatch }, value) {
         commit('setShow12HourTimeFormatMutation', value)
+        /* native javascript */
+        //localStorage.setItem('show12HourTimeFormat', value)
+
+        /* call another action within an action with dispatch */
+        dispatch('saveSettings')
+    },
+    saveSettings({ state }) {
+        /* using quasar LocalStorage plugin*/
+        LocalStorage.set('settings', state.settings)
+    },
+    getSettings({ commit }) {
+        let settings = LocalStorage.getItem('settings')
+        if(settings) {
+            commit('setSettings', settings)
+        }
     }
 }
 
