6c6e382116e3a6a44e376b008212b2c4bf710a1d
Safidy Ambinintsoa <sambinintsoa-ext@lequipe.fr>
2019-11-03 16:59:13 +0100

    Help page & open url & mailto

---
+++ src/pages/PageHelp.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-page class="q-ma-md">
+        <q-btn 
+        to="/settings"
+        flat 
+        color="primary" 
+        label="back" 
+        icon="chevron_left"
+        ></q-btn>
+        <h5>
+            This is the help page for awesome Todo
+        </h5>
+    </q-page>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <q-list bordered padding>
+    <q-list bordered padding class="q-mb-md">
       <q-item-label header>Settings</q-item-label>
 
       <q-item tag="label" v-ripple>
@@ -16,11 +16,44 @@
       </q-item>
 
     </q-list>
+
+    <q-list bordered padding>
+      <q-item-label header>More</q-item-label>
+
+      <q-item tag="label" v-ripple to="/settings/help">
+        <q-item-section>
+          <q-item-label>Help</q-item-label>
+        </q-item-section>
+        <q-item-section side >
+          <q-icon name="chevron_right"/>
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple @click="visitOurWebsite">
+        <q-item-section>
+          <q-item-label>Visit our website</q-item-label>
+        </q-item-section>
+        <q-item-section side >
+          <q-icon name="chevron_right"/>
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple @click="emailUs">
+        <q-item-section>
+          <q-item-label>Email us</q-item-label>
+        </q-item-section>
+        <q-item-section side >
+          <q-icon name="chevron_right"/>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { openURL } from 'quasar'
 export default {
   computed: {
     ...mapGetters('settingsStore', ['getSettings']),
@@ -34,7 +67,13 @@ export default {
     }
   },
   methods: {
-    ...mapActions('settingsStore', ['setShow12HourTimeFormat'])
+    ...mapActions('settingsStore', ['setShow12HourTimeFormat']),
+    visitOurWebsite() {
+      openURL('http://www.google.com')
+    },
+    emailUs() {
+      window.location.href = 'mailto:hello@me.com'
+    }
   }
 }
 </script>
